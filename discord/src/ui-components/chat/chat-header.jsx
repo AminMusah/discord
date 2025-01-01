0c841e17e8ab6c85b7c239fb68a810b3103c8126
aminmusah <ameenmusah77@gmail.com>
2025-01-01 16:07:14 +0000

    Implement socket connection and status indicator

--- discord/src/ui-components/chat/chat-header.jsx
+++ discord/src/ui-components/chat/chat-header.jsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { UserAvatar } from "../user-avatar";
+import useSocket from "../../hooks/useSocketHook";
 
 // import { MobileToggle } from "@/components/mobile-toggle";
 // import { SocketIndicator } from "@/components/socket-indicator";
@@ -7,6 +8,23 @@ import { UserAvatar } from "../user-avatar";
 // import { ChatVideoButton } from "./chat-video-button";
 
 export const ChatHeader = ({ serverId, name, type, imageUrl }) => {
+  const { connected, messages, sendMessage } = useSocket(
+    "http://localhost:6060"
+  );
+
+  const SocketIndicator = ({ status }) => {
+    return (
+      <div className="flex items-center">
+        <div
+          className={`socket-indicator mr-1 ${
+            status === "Connected" ? "bg-green-500" : "bg-red-500"
+          } w-3 h-3 rounded-full`}
+        />
+        <span className="text-xs">{status}</span>
+      </div>
+    );
+  };
+
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       {/* <MobileToggle serverId={serverId} /> */}
@@ -20,6 +38,12 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }) => {
       <div className="ml-auto flex items-center">
         {/* {type === "conversation" && <ChatVideoButton />} */}
         {/* <SocketIndicator /> */}
+
+        {connected ? (
+          <SocketIndicator status="Connected" />
+        ) : (
+          <SocketIndicator status="Disconnected" />
+        )}
       </div>
     </div>
   );
